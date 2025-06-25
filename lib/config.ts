6c7d152f5d4e786c5999994a7124c57064c7e3ba
J. Lewis <6710419+lewxdev@users.noreply.github.com>
2025-06-25 14:14:41 -0400

    fix: add missing protocol to vercel url

--- lib/config.ts
+++ lib/config.ts
@@ -1,12 +1,11 @@
 export const config = {
   // see: https://vercel.com/docs/environment-variables/framework-environment-variables#NEXT_PUBLIC_VERCEL_URL
   get vercelUrl() {
-    return process.env.NEXT_PUBLIC_VERCEL_URL;
+    const hostname = process.env.NEXT_PUBLIC_VERCEL_URL;
+    return hostname && `https://${hostname}`;
   },
 
   get url() {
-    return this.vercelUrl
-      ? `https://${this.vercelUrl}`
-      : `http://127.0.0.1:3000`;
+    return this.vercelUrl || "http://127.0.0.1:3000";
   },
 };
